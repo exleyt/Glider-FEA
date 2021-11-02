8158baf4c7ecd5fd20d81372801604d1b8a18284
exleyt <exleyt@oregonstate.edu>
2021-11-01 20:17:40 -0700

    Name change

--- compute_moment_of_inertia_matrix.m
+++ compute_moment_of_inertia_matrix.m
@@ -1,6 +1,6 @@
-function [outputArg1] = compute_moment_of_inertia_matrix()
+function [moiMatrix] = compute_moment_of_inertia_matrix()
 %COMPUTE_MOMENT_OF_INERTIA_MATRIX Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = [1,2,3;4,5,6;7,8,9];
+moiMatrix = [1,2,3;4,5,6;7,8,9];
 end
 
