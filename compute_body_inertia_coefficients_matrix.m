6f194cede53858e91b9f3b1838043a88d7088646
exleyt <exleyt@oregonstate.edu>
2021-11-01 22:53:22 -0700

    Formating

--- compute_body_inertia_coefficients_matrix.m
+++ compute_body_inertia_coefficients_matrix.m
@@ -1,7 +1,7 @@
 function [bicMatrix] = compute_body_inertia_coefficients_matrix(m,cog)
-%M  A 6x6 matrix describing the body's inertia  with 6 degrees of freedom
+% A 6x6 matrix describing the body's inertia  with 6 degrees of freedom
 % 
-% The matrix of body inertia coefficients is defined as
+% The body's inertia coefficients matrix is defined as:
 % [
 %   m       0       0       0       mzg     -myg
 %   0       m       0       -mzg    0       mxg
@@ -14,17 +14,17 @@ function [bicMatrix] = compute_body_inertia_coefficients_matrix(m,cog)
 % m is the total body mass
 % the center of gravity vector (cog) is [xg, yg, zg]
 % I is the 3x3 moment of inertia matrix
-M = diag([m,m,m]);
-A = [
-    0, m*cog(3), -m*cog(2);
-    -m*cog(3), 0, m*cog(1);
-    m*cog(2), -m*cog(1), 0;
-];
-I = compute_moment_of_inertia_matrix();
-bicMatrix = [  
-    M, A;
-    -A, I;
-];
+    M = diag([m,m,m]);
+    A = [
+        0, m*cog(3), -m*cog(2);
+        -m*cog(3), 0, m*cog(1);
+        m*cog(2), -m*cog(1), 0;
+    ];
+    I = compute_moment_of_inertia_matrix();
+    bicMatrix = [  
+        M, A;
+        -A, I;
+    ];
 end
 % TODO
 % Make compute_moment_of_inertia_matrix()
