27e2579db2221e4c867f51bcd1c5c6bba9421ff7
exleyt <exleyt@oregonstate.edu>
2021-11-01 22:53:31 -0700

    Summary comment

--- compute_moment_of_inertia_matrix.m
+++ compute_moment_of_inertia_matrix.m
@@ -1,6 +1,20 @@
 function [moiMatrix] = compute_moment_of_inertia_matrix()
-%COMPUTE_MOMENT_OF_INERTIA_MATRIX Summary of this function goes here
-%   Detailed explanation goes here
+% A 3x3 describing the body's moment of inertia
+%
+% The moment of inertia matrix is defined as:
+% [
+%  Ixx    -Ixy     -Ixz
+% -Ixy     Iyy     -Iyz
+% -Ixz    -Iyz      Izz
+% ]
+% Where 
+% Ixx = Integral(y^2+z^2)dm
+% Iyy = Integral(x^2+z^2)dm
+% Izz = Integral(x^2+y^2)dm
+% Ixy = Integral(xy)dm
+% Ixz = Integral(xz)dm
+% Iyz = Integral(yz)dm
+% m is mass
 moiMatrix = [1,2,3;4,5,6;7,8,9];
 end
 
