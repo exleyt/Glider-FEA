33253178ae7a621665574e3dbe34771ae9247e4a
exleyt <exleyt@oregonstate.edu>
2021-11-01 22:31:03 -0700

    Comment for getting all unknowns

--- main.m
+++ main.m
@@ -3,4 +3,7 @@ importGeometry(model, 'template-model\files\CORNER.STL');
 generateMesh(model);
 pdeplot3D(model)
 
-
+% M = compute_body_inertia_coefficients_matrix()
+% C = compute_hydrostatic_restoring_terms_matrix()
+% [A, B] = calculate_added_mass_and_damping_coefficient_matrices()
+% F = calculate_exciting_forces_vector()
