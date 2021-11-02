910085933f694f870e17ca951ffd057cdb0df89f
exleyt <exleyt@oregonstate.edu>
2021-11-01 20:17:59 -0700

    Added empty function

---
+++ compute_hydrostatic_restoring_terms_matrix.m
@@ -0,0 +1,6 @@
+function [hrtMatrix] = compute_hydrostatic_restoring_terms_matrix()
+%COMPUTE_HYDROSTATIC_RESTORING_TERMS_MATRIX Summary of this function goes here
+%   Detailed explanation goes here
+hrtMatrix = zeros(6,6);
+end
+
