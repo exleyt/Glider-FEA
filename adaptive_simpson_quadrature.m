13c9b0059bf3b8d97eb37f3a2cd5ba8b423fee8b
exleyt <exleyt@oregonstate.edu>
2021-11-11 01:44:57 -0800

    Added parameter e for maximum error, fixed bug
    Maybe I should just remove the limit now though since I integrate from 0->intmax rather than Inf

--- adaptive_simpson_quadrature.m
+++ adaptive_simpson_quadrature.m
@@ -1,9 +1,10 @@
-function [result] = adaptive_simpson_quadrature(f,a,b)
+function [result] = adaptive_simpson_quadrature(f,a,b,e)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+    syms k
     fa = limit(f(k),k,a);
     fb = limit(f(k),k,b);
     [m,fm,whole] = adaptive_simpson_quadrature_mem(f, a, fa, b, fb);
-    result = adaptive_simpson_quadrature_recursion(f, a, fa, b, fb, m, fm, whole);
+    result = adaptive_simpson_quadrature_recursion(f, a, fa, b, fb, m, fm, whole, e);
 end
 
--- evaluate_green_fucntion.m
+++ evaluate_green_fucntion.m
@@ -6,7 +6,7 @@ function [result] = evaluate_green_fucntion(x,y,z,rx,ry,rz,g,w)
     r1 = sqrt((x - rx)^2 + (y - ry)^2 + (z - rz)^2);
     r2 = sqrt((x - rx)^2 + (y - ry)^2 + (z + rz)^2);
     R = sqrt((x - rx)^2 + (y - ry)^2);
-    fun(k) = 1/(k-K)*exp(k*(z+rz))*besseli(0,k*R);
-    result = 1/r1 + 1/r2 + 2*K*PV*(adaptive_simpson_quadrature(fun,0,inf)) - 2*pi*1i*K*exp(K*(z + rz))*besselj(0,K*R);
+    fun(k) = 1/(k-K)*exp(k*(z+rz))*besselj(0,k*R);
+    result = 1/r1 + 1/r2 + 2*K*1*(adaptive_simpson_quadrature(fun,0,intmax,0.1)) - 2*pi*1i*K*exp(K*(z + rz))*besselj(0,K*R);
 end
 
