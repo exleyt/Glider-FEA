84acbcfb13552bacc064deb4491e21be1964cc65
exleyt <exleyt@oregonstate.edu>
2021-11-10 23:29:38 -0800

    Ready to test once asqr is made
    Still needs description

--- evaluate_green_fucntion.m
+++ evaluate_green_fucntion.m
@@ -1,10 +1,12 @@
 function [result] = evaluate_green_fucntion(x,y,z,rx,ry,rz,g,w)
 %CALCULATE_GREEN_FUCNTION Summary of this function goes here
 %   Detailed explanation goes here
+    syms PV k
     K = w^2/g;
     r1 = sqrt((x - rx)^2 + (y - ry)^2 + (z - rz)^2);
     r2 = sqrt((x - rx)^2 + (y - ry)^2 + (z + rz)^2);
     R = sqrt((x - rx)^2 + (y - ry)^2);
-    result = 1/r1 + 1/r2 - 2*pi*1i*K*exp(K*(z + rz))*besselj(0,K*R);
+    fun(k) = 1/(k-K)*exp(k*(z+rz))*besseli(0,k*R);
+    result = 1/r1 + 1/r2 + 2*K*PV*(adaptive_simpson_quadrature(fun,0,inf)) - 2*pi*1i*K*exp(K*(z + rz))*besselj(0,K*R);
 end
 
