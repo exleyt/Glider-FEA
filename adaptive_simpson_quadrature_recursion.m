603bac7a3808048fd2a1c9828eb6d103f717649d
exleyt <exleyt@oregonstate.edu>
2021-11-11 01:56:16 -0800

    implemented function but fails on line 7
    can't compare a number to a symbolic equation

--- adaptive_simpson_quadrature_recursion.m
+++ adaptive_simpson_quadrature_recursion.m
@@ -1,6 +1,14 @@
-function [outputArg1,outputArg2] = adaptive_simpson_quadrature_recursion(f,a,fa,b,fb,m,fm,whole)
+function [result] = adaptive_simpson_quadrature_recursion(f,a,fa,b,fb,m,fm,whole,e)
 %ADAPTIVE_SIMPSON_QUADRATURE_RECURSION Summary of this function goes here
 %   Detailed explanation goes here
-    
+    [lm,flm,left] = adaptive_simpson_quadrature_mem(f,a,fa,m,fm);
+    [rm,frm,right] = adaptive_simpson_quadrature_mem(f,m,fm,b,fb);
+    delta = left + right - whole;
+    if abs(delta) <= 15 * e
+        result = left + right + delta / 15;
+    else
+        result = adaptive_simpson_quadrature_recursion(f,a,fa,m,fm,lm,flm,left,e/2);
+        result = result + adaptive_simpson_quadrature_recursion(f,m,fm,b,fb,rm,frm,right,e/2); 
+    end
 end
 
