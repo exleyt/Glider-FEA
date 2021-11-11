ff53756c224e8b17431533a01eb5c7d72843534a
exleyt <exleyt@oregonstate.edu>
2021-11-10 23:28:27 -0800

    New file created
    basically empty

---
+++ adaptive_simpson_quadrature_recursion.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = adaptive_simpson_quadrature_recursion(f,a,fa,b,fb,m,fm,whole)
+%ADAPTIVE_SIMPSON_QUADRATURE_RECURSION Summary of this function goes here
+%   Detailed explanation goes here
+    
+end
+
