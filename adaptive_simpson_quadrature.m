2f5030a292f50fa17f8a00cbd5cf2e8e644ca96a
exleyt <exleyt@oregonstate.edu>
2021-11-13 05:41:17 -0800

    don't need limits since no values will be udef or inf

--- adaptive_simpson_quadrature.m
+++ adaptive_simpson_quadrature.m
@@ -2,8 +2,8 @@ function [result] = adaptive_simpson_quadrature(f,a,b,e)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     syms k
-    fa = limit(f(k),k,a);
-    fb = limit(f(k),k,b);
+    fa = f(a);
+    fb = f(b);
     [m,fm,whole] = adaptive_simpson_quadrature_mem(f, a, fa, b, fb);
     result = adaptive_simpson_quadrature_recursion(f, a, fa, b, fb, m, fm, whole, e);
 end
