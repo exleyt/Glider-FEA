896b9b26527cd9c962fecf177a296f1d06bf9147
exleyt <exleyt@oregonstate.edu>
2021-11-15 20:28:27 -0800

    Separates surface triangles of mesh

--- main.m
+++ main.m
@@ -1,6 +1,29 @@
+% Creates a new pde object
 model = createpde();
-importGeometry(model, 'template-model\files\CORNER.STL');
-generateMesh(model);
-pdeplot3D(model)
 
+% Generates a discrete geometry object from stl file
+importGeometry(model,"template-model\files\CORNER.STL");
 
+% Generates a finite element tetrahedra mesh object of the geometry
+generateMesh(model, 'GeometricOrder','linear','Hmin',1);
+
+% Plots mesh 
+%pdeplot3D(model);
+
+% Makes a traingulation object of mesh
+mto = triangulation(model.Mesh.Elements.', model.Mesh.Nodes.');
+
+% Makes a traingle connectivity and point list of surface triangles
+[T, P] = freeBoundary(mto);
+
+% Makes a traingulation object of mesh surface
+to = triangulation(T, P);
+
+% Plots traingulation object
+trimesh(t);
+
+% Sets N equal to the number of surface triangles
+[N,~] = size(T);
+
+% Declares the pde as a system of equations N equations
+model.PDESystemSize = N;
