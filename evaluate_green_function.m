6e2cc2cd681204ca43db34357ef64e4719c80f8c
exleyt <exleyt@oregonstate.edu>
2021-11-30 03:44:00 -0800

    Minor optimization and added e,d to parameters

--- evaluate_green_function.m
+++ evaluate_green_function.m
@@ -1,4 +1,4 @@
-function [result] = evaluate_green_function(x,xi,K)
+function [result] = evaluate_green_function(x,xi,K,e,d)
 % Numerically calculates the Green Function hdl.handle.net/20.500.12489/844
 %
 % Finds the complex valued influence of a pulsating source located at xi on
@@ -6,16 +6,14 @@ function [result] = evaluate_green_function(x,xi,K)
 % K=w^2/g
 % w is the waves angular frequency
 % g is acceleration due to gravity
-    % An error value for asq. Needs optimization! Too low and all values
-    % become 0. Too high and results will suffer.
-    e = 10 ^ -6; 
+% e is an error value for asq estimation. Needs optimization!
+% d is a maximum depth for asq estimation. Needs optimization!
     % An asq estimation of infinity. Should be around the value that fun->0
-    infi = 20; 
-    % A maximum depth for asq estimation.
-    d = 20;
+    % Needs optimization, but not essential. It saves very little time.
+    infi = 100;
     r1 = sqrt((x(1) - xi(1))^2 + (x(2) - xi(2))^2 + (x(3) - xi(3))^2);
     r2 = sqrt((x(1) - xi(1))^2 + (x(2) - xi(2))^2 + (x(3) + xi(3))^2);
     R = sqrt((x(1) - xi(1))^2 + (x(2) - xi(2))^2);
     fun = @(k) besselj(0,k*R)*exp(k*(x(3)+xi(3)))/(k-K);
-    result = 1/r1 + 1/r2 + 2*K*1*(asq(fun,0,K-e,e,d) + asq(fun,K+e,K+e+1,e,d) + asq(fun,K+e+1,infi,e,d)) - 2*pi*1i*K*exp(K*(x(3) + xi(3)))*besselj(0,K*R);
+    result = 1/r1 + 1/r2 + 2*K*1*(asq(fun,0,K-e,e,d) + asq(fun,K+e,infi,e,d)) - 2*pi*1i*K*exp(K*(x(3) + xi(3)))*besselj(0,K*R);
 end
