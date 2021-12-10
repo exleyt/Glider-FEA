babacde261dc6f0c518abb5045bbe2a3c2c64a7d
exleyt <exleyt@oregonstate.edu>
2021-12-09 22:44:16 -0800

    More accurate directional derivatives

--- evaluate_green_function_partial_xi.m
+++ evaluate_green_function_partial_xi.m
@@ -1,8 +1,8 @@
-function [result] = evaluate_green_function_partial_xi(x,xi,K,e,d,j,e2,f)
+function [result] = evaluate_green_function_partial_xi(x,xi,K,e,d,j,f)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    xi(j) = xi(j) - e2;
+    xi(j) = xi(j) - e;
     fxix = evaluate_green_function(x,xi,K,e,d);
-    result = (fxix - f) / e2;
+    result = (fxix - f) / e;
 end
 
--- evaluate_green_function_partial_xinormal.m
+++ evaluate_green_function_partial_xinormal.m
@@ -4,9 +4,9 @@ function [df] = evaluate_green_function_partial_xinormal(x,xi,K,e,d,n)
     grad = zeros(3,1);
     f = evaluate_green_function(x,xi,K,e,d);
     e2 = e^2;
-    grad(1) = evaluate_green_function_partial_xi(x,xi,K,e,d,1,e2,f);
-    grad(2) = evaluate_green_function_partial_xi(x,xi,K,e,d,2,e2,f);
-    grad(3) = evaluate_green_function_partial_xi(x,xi,K,e,d,3,e2,f);
+    grad(1) = evaluate_green_function_partial_xi(x,xi,K,e2,d,1,f);
+    grad(2) = evaluate_green_function_partial_xi(x,xi,K,e2,d,2,f);
+    grad(3) = evaluate_green_function_partial_xi(x,xi,K,e2,d,3,f);
     df = dot(grad, n);
 end
 
