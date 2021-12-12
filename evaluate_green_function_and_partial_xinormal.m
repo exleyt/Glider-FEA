bd2f69e0812855268412a53e67d9dac32bad7ec9
exleyt <exleyt@oregonstate.edu>
2021-12-11 22:02:22 -0800

    Got rid of e2, e is g decent step for estimating the derivative

--- evaluate_green_function_and_partial_xinormal.m
+++ evaluate_green_function_and_partial_xinormal.m
@@ -1,12 +1,11 @@
-function [f,df] = evaluate_green_function_and_partial_xinormal(x,xi,K,e,d,n)
+function [f,df] = evaluate_green_function_and_partial_xinormal(x,xi,n,K,e,d)
 %EVALUATE_GREEN_FUNCTION_NORMAL_DERIVATIVE Summary of this function goes here
 %   Detailed explanation goes here
     grad = zeros(3,1);
     f = evaluate_green_function(x,xi,K,e,d);
-    e2 = e^2;
-    grad(1) = evaluate_green_function_partial_xi(x,xi,K,e2,d,1,f);
-    grad(2) = evaluate_green_function_partial_xi(x,xi,K,e2,d,2,f);
-    grad(3) = evaluate_green_function_partial_xi(x,xi,K,e2,d,3,f);
+    for j=1:3
+        grad(j) = evaluate_green_function_partial_xi(x,xi,K,e,d,j,f);
+    end
     df = dot(grad, n);
 end
 
