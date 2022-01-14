eb25533849f7fd72f4ec7c37f52659019e735a93
exleyt <exleyt@oregonstate.edu>
2022-01-13 22:59:54 -0800

    Renamed variables

--- calculate_velocity_potential_vector.m
+++ calculate_velocity_potential_vector.m
@@ -1,11 +1,11 @@
-function [phi] = calculate_velocity_potential_vector(N,C,F,F6,T,K)
+function [phi] = calculate_velocity_potential_vector(S,C,N,N6,T,K)
 %CALCULATE_VELOCITY_POTENTIAL_VECTOR Summary of this function goes here
 %   Detailed explanation goes here
 
     % Defines the surface integral terms
-    Gnks = zeros(N,N); % All N^2 Gnk
-    Gnks_sum = zeros(N,6); % All N sums of Gnk * Fk as 6-dimensional vectors 
-    Mnks = zeros(N,N); % All N^2 Mnk
+    Gnks = zeros(S,S); % All N^2 Gnk
+    Gnks_sum = zeros(S,6); % All N sums of Gnk * Fk as 6-dimensional vectors 
+    Mnks = zeros(S,S); % All N^2 Mnk
     
     % Defines values for guasian quadrature
     x = [sqrt(3/7 - 2/7 * sqrt(6/5)); -sqrt(3/7 - 2/7 * sqrt(6/5)); ...
@@ -14,20 +14,20 @@ function [phi] = calculate_velocity_potential_vector(N,C,F,F6,T,K)
          (18 - sqrt(30))/36;(18 - sqrt(3))/36];
 
     % Fills Gnks and Mnks
-    parfor k = 1:N
-        for n = 1:N
+    parfor k = 1:S
+        for n = 1:S
             [Gnks(n,k),Mnks(n,k)] = estimate_surface_integral_GM(C(n,:), ...
-                T(:,:,k),F(k,:),K,x,w);
+                T(:,:,k),N(k,:),K,x,w);
         end
     end
-    for n = 1:N
-        for k = 1:N
-            Gnks_sum(n,:) = Gnks_sum(n,:) + Gnks(n,k) * F6(k,:);
+    for n = 1:S
+        for k = 1:S
+            Gnks_sum(n,:) = Gnks_sum(n,:) + Gnks(n,k) * N6(k,:);
         end
         Mnks(n,n) = Mnks(n,n) + 2*pi;
     end
     
-    phi = zeros(N,6);
+    phi = zeros(S,6);
     for j = 1:6
         phi(:,j) = linsolve(Mnks,Gnks_sum(:,j));
     end
