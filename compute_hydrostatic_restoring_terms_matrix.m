5268d7c7205846993f8dd37ef1cfaf04cdf20bd8
exleyt <exleyt@oregonstate.edu>
2022-01-16 21:05:48 -0800

    Extremely simple HRT

--- compute_hydrostatic_restoring_terms_matrix.m
+++ compute_hydrostatic_restoring_terms_matrix.m
@@ -1,6 +1,15 @@
-function [hrtMatrix] = compute_hydrostatic_restoring_terms_matrix()
+function [C] = compute_hydrostatic_restoring_terms_matrix(pm,g)
 % A 6x6 matrix describing the body's hydrostatic restoring force
 %
-hrtMatrix = zeros(6,6);
+    C = zeros(6,6);
+    cog = zeros(3,1); 
+    for j = 1:3
+        cog(j) = sum(pm(j+1,1:N)) / N;
+    end
+    m = sum(pm(1,1:N));
+    C(4,4) = -m*g*cog(2);
+    C(4,5) = g*m*cog(1);
+    C(6,5) = g*m*cog(3);
+    C(6,6) = -m*g*cog(2);
 end
 
