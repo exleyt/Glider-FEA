c69c2ac8db22ea502b29c9f59371bd28bbe50c1a
exleyt <exleyt@oregonstate.edu>
2022-01-27 22:19:36 -0800

    Fixed Guassian ???
    It should run ~16 times faster and be correct for values of G at least, but something is wrong when linsolving since rcond ~ 0

--- velocityPotential.m
+++ velocityPotential.m
@@ -6,18 +6,60 @@ function [phi] = velocityPotential(S,C,N,N6,T,K)
     Gnks = zeros(S,S); % All N^2 Gnk
     Gnks_sum = zeros(S,6); % All N sums of Gnk * Fk as 6-dimensional vectors 
     Mnks = zeros(S,S); % All N^2 Mnk
-    
-    % Defines values for guasian quadrature
-    x = [sqrt(3/7 - 2/7 * sqrt(6/5)); -sqrt(3/7 - 2/7 * sqrt(6/5)); ...
-         sqrt(3/7 + 2/7 * sqrt(6/5)); -sqrt(3/7 + 2/7 * sqrt(6/5))];
-    w = [(18 + sqrt(30))/36;(18 + sqrt(300))/36; ...
-         (18 - sqrt(30))/36;(18 - sqrt(3))/36];
+
+    % Guassian weights
+    w3 = 1/3;
+    w7 = [0.1259391805448;
+            0.1259391805448;
+            0.1259391805448;
+            0.1323941527885;
+            0.1323941527885;
+            0.1323941527885;
+            0.225];
+
+    oneSixth = 1/6;
 
     % Fills Gnks and Mnks
-    parfor k = 1:S
-        for n = 1:S
-            [Gnks(n,k),Mnks(n,k)] = surfaceIntegralGMOuter(C(n,:), ...
-                T(:,:,k),N(k,:),K,x,w);
+    for k = 1:S
+        % Paremeterizes triangle to (u,v)
+        Tk = T(:,:,k);
+        ru = Tk(2,:) - Tk(1,:);
+        rv = Tk(3,:) - Tk(1,:);
+
+        % The area of the traingle
+        A = 0.5*norm(cross(ru,rv));
+        
+        % The guassian points to evaluate at
+        r3 = [oneSixth*ru + oneSixth*rv; 
+              2/3*ru + oneSixth*rv; 
+              oneSixth*ru + 2/3*rv] + Tk(1,:);
+        r7 = [0.1012865073235*ru + 0.1012865073235*rv; 
+              0.7974269853531*ru + 0.1012865073235*rv;
+              0.1012865073235*ru + 0.7974269853531*rv;
+              0.4701420641051*ru + 0.0597158717898*rv;
+              0.4701420641051*ru + 0.4701420641051*rv;
+              0.0597158717898*ru + 0.4701420641051*rv;
+              0.3333333333333*ru + 0.3333333333333*rv];
+        
+        for n = 1:S 
+            if n~=k
+                % Sums each guassian function evaulation
+                for m = 1:3
+                    [fg,fm] = greenFunctionAndPartialXINormal(C(n,:),r3(m,:),N(k,:),K);
+                    Gnks(n,k) = Gnks(n,k) + fg*w3;
+                    Mnks(n,k) = Mnks(n,k) + fm*w3;
+                end
+            % More intensive guassian for the case when n=k
+            else
+                % Sums each guassian function evaulation
+                for m = 1:7
+                    [fg,fm] = greenFunctionAndPartialXINormal(C(n,:),r7(m,:),N(k,:),K);
+                    Gnks(n,k) = Gnks(n,k) + fg*w7(m);
+                    Mnks(n,k) = Mnks(n,k) + fm*w7(m);
+                end
+            end
+            Gnks(n,k) = A*Gnks(n,k);
+            Mnks(n,k) = A*Mnks(n,k);
         end
     end
     for n = 1:S
@@ -32,4 +74,3 @@ function [phi] = velocityPotential(S,C,N,N6,T,K)
         phi(:,j) = linsolve(Mnks,Gnks_sum(:,j));
     end
 end
-
