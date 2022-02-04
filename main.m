37f47e5cc43280c0f85d0555ed3eee551aae4461
exleyt <exleyt@oregonstate.edu>
2022-02-03 23:06:27 -0800

    Better plot and smaller model

--- main.m
+++ main.m
@@ -3,16 +3,16 @@
 addpath('asq');
 
 % Makes a traingulation object of the mesh
-to = stlread("models/Glider v8.stl");
+to = stlread("models/Glider v9.stl");
+
+% For some reason the model is read 10 times too small???
+to = triangulation(to.ConnectivityList, to.Points * 10);
 
 % FFR: Plots traingulation object
-figure(10);
+figure();
 trimesh(to);
 hold on
 axis equal
-
-% Records the number of surface triangles
-[N,~] = size(to);
 
 % Finds each triangle's center point and face normal vectors
 CP  = incenter(to);
@@ -21,6 +21,9 @@ FN = faceNormal(to);
 % FFR: Plotes the triangulation object's normals
 quiver3(CP(:,1),CP(:,2),CP(:,3), ...
      FN(:,1),FN(:,2),FN(:,3),0.5,'color','r');
+
+% Records the number of surface triangles
+[N,~] = size(to);
 
 % Creates each triangle's 6-dimensional normal vector
 FN6 = zeros(N,3);
@@ -43,6 +46,11 @@ pm = [9,0,0,-0.3
     18,0,1.2,-0.12014646;
     18,0,1.4,-0.531422;
     9,0,1.7,0]; % point masses along body length (x)
+Ts = [2]; %union(2:10,4:0.2:6); % list of wave periods 
+[~,nT] = size(Ts);
+thetas = [0,pi()/2,pi()]; % list of incident angles
+thetac = ["o","+","*"]; % list of plot modifiers for each theta
+[~,ntheta] = size(thetas);
 
 % Defines a list of N triangles so that parfor can nicely slice data
 Tri = zeros(3,3,N);
@@ -53,38 +61,49 @@ end
 M = bodyInertiaMatrix(pm);
 C = hydrostaticRestoringMatrix(pm,g);
 
-for T = 1:10
-    w = 2*pi()/T;
-    K = w^2/g;
+phis = zeros(nT,N,6);
+RAOs = zeros(nT,ntheta,6);
 
-    phi = velocityPotential(N,CP,FN,FN6,Tri,K);
-    [A,B] = addedMassAndDampingMatrices(Tri,phi,FN,p,w);
+for j = 1:nT
+    w = 2*pi()/Ts(j); % sets angular frequency
+    K = w^2/g; % sets K(w)
 
+
+    phis(j,:,:) = velocityPotential(N,CP,FN,FN6,Tri,K);
+    [A,B] = addedMassAndDampingMatrices(Tri,phis(j,:,:),FN,p,w);
+
+    for i = 1:ntheta
+        [F] = excitingForce(Tri,phis(j,:,:),FN,p,k,g,thetas(i)); 
     
-    angle = [0,pi()/2,pi()]; % incident wave angle
-    shape = ["o","+","*"]; % shape of point at for each angle
-
-    % Loops through different incident wave angles
-    for j = 1:3
-        [F] = excitingForce(Tri,phi,FN,p,k,g,angle(j));
-        
         nu = linsolve(-w^2*(M + A) + 1i*w*B + C,F);
+    
         H = nu / a; % response function
-        RAO = abs(H); % response amplitude operator
-   
-        % Plots surge RAO on fig1
-        figure(1)
-        hold on
-        plot(T, RAO(1), shape(j));
-        
-        % Plots sway RAO on fig2
-        figure(2)
-        hold on
-        plot(T, RAO(2), shape(j));
+        RAOs(j,i,:) = abs(H); % response amplitude operator
+    end
+end
 
-        % Plots heave RAO on fig3
-        figure(3)
-        hold on
-        plot(T, RAO(3), shape(j));
+figure()
+hold on
+tl = tiledlayout(3,1);
+
+ax1 = nexttile;
+title(ax1,'Heave')
+ax2 = nexttile;
+title(ax2,'Surge')
+ax3 = nexttile;
+title(ax3,'Sway')
+
+linkaxes([ax1,ax2,ax3],'x');
+xlim([0.5 10.5])
+ylim([0 inf])
+xlabel(tl,'RAO')
+ylabel(tl,'Period')
+t.TileSpacing = 'compact';
+
+for j = 1:nT
+    for i = 1:ntheta
+        plot(ax1,Ts(j),RAOs(j,1,3),thetac(i))
+        plot(ax2,Ts(j),RAOs(j,1,1),thetac(i))
+        plot(ax3,Ts(j),RAOs(j,1,2),thetac(i))
     end
 end
