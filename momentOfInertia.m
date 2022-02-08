aadd4521a2cfd74b025cb77e89094d0a1407151a
exleyt <exleyt@oregonstate.edu>
2022-02-07 19:16:42 -0800

    Fixed MoI for loop

--- momentOfInertia.m
+++ momentOfInertia.m
@@ -1,4 +1,4 @@
-function [moiMatrix] = momentOfInertia(pointMasses)
+function [result] = momentOfInertia(pm)
 % A 3x3 describing the body's moment of inertia
 %
 % The moment of inertia matrix is the sum of the matrices: 
@@ -6,42 +6,36 @@ function [moiMatrix] = momentOfInertia(pointMasses)
 %   Ixx     Ixy     Ixz
 %   Ixy     Iyy     Iyz
 %   Ixz     Iyz     Izz
-% ]
-
-% Where
-% Ixx = Sum(1,j,(yj^2 + zj^2)mj)
-% Iyy = Sum(1,j,(xj^2 + zj^2)mj)
-% Izz = Sum(1,j,(xj^2 + yj^2)mj)
-% Ixy = -Sum(1,j,xj * yj * mj)
-% Ixz = -Sum(1,j,xj * zj * mj)
-% Iyz = -Sum(1,j,yj * zj * mj)   
-% pointMasses is the matrix of [mjs,pjs]
+% ] s.t.
+% cog is the horizontal center of gravity vector
+% Ixx = Sum(mj*(yj^2 + zj^2))
+% Iyy = Sum(mj*(xj^2 + zj^2))
+% Izz = Sum(mj*(xj^2 + yj^2))
+% Ixy = -Sum(mj*xj*yj)
+% Ixz = -Sum(mj*xj*zj)
+% Iyz = -Sum(mj*yj*zj)   
+% pm is the matrix of [mjs,pjs]
 % mj is the jth point mass
 % pj is the position vector of the jth point mass (xj,yj,zj)
-%
-% It is assumed that the glider can be treated as a sum of point masses and
-% that pointMasses are have positions relative to the gliders center of
-% mass
-    xx = 0;
-    yy = 0;
-    zz = 0;
-    xy = 0;
-    xz = 0;
-    yz = 0;
-    for pm = pointMasses
-        xx = xx + pm(1)*(pm(3)^2 + pm(4)^2);
-        yy = yy + pm(1)*(pm(2)^2 + pm(4)^2);
-        zz = zz + pm(1)*(pm(2)^2 + pm(3)^2);
-        xy = xy + pm(1)*pm(2)*pm(3);
-        xz = xz + pm(1)*pm(2)*pm(4);
-        yz = yz + pm(1)*pm(3)*pm(4);
+% It is assumed that the glider can be treated as a sum of point masses
+    Ixx = 0;
+    Iyy = 0;
+    Izz = 0;
+    Ixy = 0;
+    Ixz = 0;
+    Iyz = 0;
+    [j,~] = size(pm);
+    for i = 1:j
+        Ixx = Ixx + pm(i,1)*(pm(i,3)^2 + pm(i,4)^2);
+        Iyy = Iyy + pm(i,1)*(pm(i,2)^2 + pm(i,4)^2);
+        Izz = Izz + pm(i,1)*(pm(i,2)^2 + pm(i,3)^2);
+        Ixy = Ixy + pm(i,1)*pm(i,2)*pm(i,3);
+        Ixz = Ixz + pm(i,1)*pm(i,2)*pm(i,4);
+        Iyz = Iyz + pm(i,1)*pm(i,3)*pm(i,4);
     end
-    moiMatrix = [
-        xx, -xy, -xz;
-       -xy,  yy, -yz;
-       -xz, -yz,  zz;
+    result = [
+        Ixx, -Ixy, -Ixz;
+       -Ixy,  Iyy, -Iyz;
+       -Ixz, -Iyz,  Izz;
     ];
 end
-% TODO 
-% Since the glider actually rotates about its center of buoyancy, pjs
-% should probably be given in relation to that instead.
