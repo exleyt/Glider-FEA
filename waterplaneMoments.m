0edde71df8d9d152fb0c4e210f4f513f1d3c4eff
exleyt <exleyt@oregonstate.edu>
2022-02-24 04:49:30 -0800

    Renaming variables

--- waterplaneMoments.m
+++ waterplaneMoments.m
@@ -1,19 +1,20 @@
-function [S,CF,S11,S22] = waterplaneMoments(CL,P)
+function [S,CF,Ixx,Iyy] = waterplaneMoments(CL,P)
 % Returns the waterplane area and moments of a list of triangles
 %
 % Given a list of point P(n,3) and connectivity list of triangles CL(N,3)
 %  returns: 
 % S the area of the waterplane
-% CF the waterplane's center of flotation [S1/S,S2/S,0] where:
-%  Sj = the surface integral of xj
-% S11 and S22 where:
-%  Sij =  the surface integral of xi*xj
+% CF the waterplane's center of flotation [Ix/S,Iy/S,0] where:
+%  Ii =  the surface integral of (xi) 
+% Ixx and Ixx where:
+%  Iij =  the surface integral of (xi*xj)
     S = 0;
     CF = zeros(3,1); % [S1,S2,0]/S
-    S11 = 0;
-    S22 = 0;
-
+    Ixx = 0;
+    Iyy = 0;
     [N,~] = size(CL);
+
+    % Sums discretize elements of the waterplane
     for j = 1:N
         % Parameterized triangle
         r0 = P(CL(j,1),:);
@@ -25,14 +26,12 @@ function [S,CF,S11,S22] = waterplaneMoments(CL,P)
 
         S = S + 0.5*A;
 
-        % S1
         CF(1) = CF(1) + A*(3*r0(1) + ru(1) + rv(1))/6;
-        % S2
         CF(2) = CF(2) + A*(3*r0(2) + ru(2) + rv(2))/6;
         
-        S11 = S11 + A*(6*r0(1)^2 + 4*(ru(1) + rv(1))*r0(1) + ru(1)^2 + ...
+        Ixx = Ixx + A*(6*r0(1)^2 + 4*(ru(1) + rv(1))*r0(1) + ru(1)^2 + ...
             ru(1)*rv(1) + rv(1)^2) / 12;
-        S22 = S22 + A*(6*r0(2)^2 + 4*(ru(2) + rv(2))*r0(2) + ru(2)^2 + ...
+        Iyy = Iyy + A*(6*r0(2)^2 + 4*(ru(2) + rv(2))*r0(2) + ru(2)^2 + ...
             ru(2)*rv(2) + rv(2)^2) / 12;
     end
 
