7699858c54f267fe5c96d42f444ae4b64e1d6dd1
exleyt <exleyt@oregonstate.edu>
2022-03-05 04:19:47 -0800

    Initial Commit

---
+++ testNormals.m
@@ -0,0 +1,45 @@
+classdef testNormals < matlab.unittest.TestCase
+
+    properties
+        epsilon
+    end    
+ 
+    methods(TestMethodSetup)
+        function defineEpsilon(testCase)
+            testCase.epsilon = 1E-6;
+        end
+    end
+ 
+    methods(TestMethodTeardown)
+    end
+    
+    methods (Test)
+        function testNormal6DOF1(testCase)
+            [FN6] = normal6DOF([1,1,1],[0,0,0]);
+            value = [0,0,0,0,0,0];
+
+            testCase.verifyEqual(FN6,value);
+        end
+
+        function testNormal6DOF2(testCase)
+            [FN6] = normal6DOF([0,0,0],[1,1,1]);
+            value = [1,1,1,0,0,0];
+
+            testCase.verifyEqual(FN6,value);
+        end
+
+        function testNormal6DOF3(testCase)
+            [FN6] = normal6DOF([1,1,1],[2,3,4]);
+            value = [2,3,4,1,-2,1];
+
+            testCase.verifyEqual(FN6,value);
+        end
+
+        function testNormal6DOF4(testCase)
+            [FN6] = normal6DOF([2,3,4],[1,1,1]);
+            value = [1,1,1,-1,2,-1];
+
+            testCase.verifyEqual(FN6,value);
+        end
+    end
+end
