fec63fc8b46be7634b40805168e4257041f15deb
exleyt <exleyt@oregonstate.edu>
2022-04-21 19:34:58 -0700

    New HRT
    Also fixed a bug (wrong function name) in the last update in AUV_FEA

--- hydrostaticRestoringMatrix.m
+++ hydrostaticRestoringMatrix.m
@@ -1,4 +1,4 @@
-function [C] = hydrostaticRestoringMatrix(pm,g,p,model,face)
+function [C] = hydrostaticRestoringMatrix(pm,g,p,model,mesh)
 % A 6x6 matrix describing the body's hydrostatic restoring force
 %
 % The matrix C is made up of the terms where
@@ -15,23 +15,25 @@ function [C] = hydrostaticRestoringMatrix(pm,g,p,model,face)
 %
 % This is from a textbook where it is assumed that the origin is the center
 %  of flotation which it very much is not as currently defined
-    [CL,P] = waterplaneTriangulation(model.Mesh,face);
-    [S,CF,Ixx,Iyy] = waterplaneMoments(CL,P);
+    %[CL,P] = waterplaneTriangulation(model.Mesh,face);
+    FN = -faceNormal(mesh);
+    [S,Sx,Sy,Sxy,Sxx,Syy] = waterplaneMoments(mesh.ConnectivityList,mesh.Points,FN);
     [V,CB] = volumeMoments(model.Mesh);
     [m,Cg] = massMoments(pm);
 
-    Ch = p*g*S; % force per unit distance
+    pg = p*g;
     W = m*g; % weight
-    Fb = p*g*V; % buoyancy force
-    C44 = -W*Cg(3) + Fb*CB(3) + p*g*Ixx + Ch*CF(2)^2;
-    C55 = -W*Cg(3) + Fb*CB(3) + p*g*Iyy + Ch*CF(1)^2;
+    Fb = pg*V; % buoyancy force
+    Fd = Fb*CB - W*Cg;
+    C44 = pg*Syy + Fd(3);
+    C55 = pg*Sxx + Fd(3);
 
     C = [
-        0,  0,  0,          0,                  0,                  0;
-        0,  0,  0,          0,                  0,                  0;
-        0,  0,  Ch,         Ch*CF(2),           -Ch*CF(1),          0;
-        0,  0,  Ch*CF(2),   C44,                -Ch*CF(1)*CF(2),    (W*Cg(1) - Fb*CB(1));
-        0,  0,  -Ch*CF(1),  -Ch*CF(1)*CF(2),    C55,                (W*Cg(2) - Fb*CB(2));
-        0,  0,  0,          0,                  0,                  0;
+        0,  0,  0,      0,      0,          0;
+        0,  0,  0,      0,      0,          0;
+        0,  0,  pg*S,   pg*Sy,  -pg*Sx,     0;
+        0,  0,  0,      C44,    -pg*Sxy,    -Fb*CB(1) + W*Cg(3);
+        0,  0,  0,      0,      C55,        -Fd(2);
+        0,  0,  0,      0,      0,          0;
     ];
 end
--- waterplaneMoments.m
+++ waterplaneMoments.m
@@ -1,4 +1,4 @@
-function [S,CF,Ixx,Iyy] = waterplaneMoments(CL,P)
+function [S,Sx,Sy,Sxy,Sxx,Syy] = waterplaneMoments(CL,P,FN)
 % Returns the waterplane area and moments of a list of triangles
 %
 % Given a list of point P(n,3) and connectivity list of triangles CL(N,3)
@@ -9,9 +9,11 @@ function [S,CF,Ixx,Iyy] = waterplaneMoments(CL,P)
 % Ixx and Ixx where:
 %  Iij =  the surface integral of (xi*xj)
     S = 0;
-    CF = zeros(3,1); % [S1,S2,0]/S
-    Ixx = 0;
-    Iyy = 0;
+    Sx = 0;
+    Sy = 0;
+    Sxy = 0;
+    Sxx = 0;
+    Syy = 0;
     [N,~] = size(CL);
 
     % Sums discretize elements of the waterplane
@@ -24,17 +26,16 @@ function [S,CF,Ixx,Iyy] = waterplaneMoments(CL,P)
         % Area of parallelogram
         A = norm(cross(ru,rv));
 
-        S = S + 0.5*A;
-
-        CF(1) = CF(1) + A*(3*r0(1) + ru(1) + rv(1))/6;
-        CF(2) = CF(2) + A*(3*r0(2) + ru(2) + rv(2))/6;
-        
-        Ixx = Ixx + A*(6*r0(1)^2 + 4*(ru(1) + rv(1))*r0(1) + ru(1)^2 + ...
+        S = S + 0.5*A*FN(j,3);
+        Sx = Sx + A*FN(j,3)*(3*r0(1) + ru(1) + rv(1))/6;
+        Sy = Sy + A*FN(j,3)*(3*r0(2) + ru(2) + rv(2))/6;
+        Sxy = Sxy + A*FN(j,3)*(4*r0(1)*(3*r0(2) + ru(2) + rv(2)) ...
+            + 4*r0(2)*(ru(1) + rv(1)) + ru(1)*(2*ru(2) + rv(2)) ...
+            + rv(1)*(ru(2) + 2*rv(2)))/24;
+        Sxx = Sxx + A*FN(j,3)*(6*r0(1)^2 + 4*(ru(1) + rv(1))*r0(1) + ru(1)^2 + ...
             ru(1)*rv(1) + rv(1)^2) / 12;
-        Iyy = Iyy + A*(6*r0(2)^2 + 4*(ru(2) + rv(2))*r0(2) + ru(2)^2 + ...
+        Syy = Syy + A*FN(j,3)*(6*r0(2)^2 + 4*(ru(2) + rv(2))*r0(2) + ru(2)^2 + ...
             ru(2)*rv(2) + rv(2)^2) / 12;
     end
-
-    CF = CF/S;
 end
 
