2177f039f8ded01da9d2cf4f9d75fee7e55b7ad2
exleyt <exleyt@oregonstate.edu>
2022-05-05 18:49:16 -0700

    Removed most waterplaneMoments tests

--- tests/testWaterplanes.m
+++ tests/testWaterplanes.m
@@ -92,102 +92,34 @@ classdef testWaterplanes < matlab.unittest.TestCase
             testCase.verifyLessThan(abs(sum(Z) - 3*Ze),testCase.epsilon);
         end
 
-        function testWaterplaneTriangulationAndMoments1(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,2);
-            [S,~,~,~] = waterplaneMoments(CL,P);
+        function testWaterplaneMoments1(testCase)
+            TO = triangulation(testCase.ModelSqr1.Mesh.Elements.', testCase.ModelSqr1.Mesh.Nodes.');
+            [CL,P] = freeBoundary(TO);
+            to = triangulation(CL,P);
+            FN = -faceNormal(to);
+            [S,~,~,~,~,~] = waterplaneMoments(CL,P,FN);
 
-            testCase.verifyLessThan(abs(S - 1),testCase.epsilon);
+            testCase.verifyLessThan(abs(S),testCase.epsilon);
         end
 
-        function testWaterplaneTriangulationAndMoments2(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,2);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
+        function testWaterplaneMoments2(testCase)
+            TO = triangulation(testCase.ModelSqr1.Mesh.Elements.', testCase.ModelSqr1.Mesh.Nodes.');
+            [CL,P] = freeBoundary(TO);
+            to = triangulation(CL,P);
+            FN = -faceNormal(to);
+            [~,Sx,~,~,~,~] = waterplaneMoments(CL,P,FN);
 
-            testCase.verifyLessThan(abs(CF - [0;0;0]),testCase.epsilon);
+            testCase.verifyLessThan(abs(Sx),testCase.epsilon);
         end
 
-        function testWaterplaneTriangulationAndMoments3(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,3);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
+        function testWaterplaneMoments3(testCase)
+            TO = triangulation(testCase.ModelSqr1.Mesh.Elements.', testCase.ModelSqr1.Mesh.Nodes.');
+            [CL,P] = freeBoundary(TO);
+            to = triangulation(CL,P);
+            FN = -faceNormal(to);
+            [~,~,Sy,~,~,~] = waterplaneMoments(CL,P,FN);
 
-            testCase.verifyLessThan(abs(CF - [0.5;0;0]),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments4(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,6);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(CF - [0;-0.50;0]),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments5(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,2);
-            [~,~,Ixx,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(Ixx-1/12),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments6(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelSqr1.Mesh,2);
-            [~,~,~,Iyy] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(Iyy-1/12),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments7(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,2);
-            [S,~,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(S - 8),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments8(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,3);
-            [S,~,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(S - 12),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments9(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,4);
-            [S,~,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(S - 6),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments10(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,2);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(CF - [0;0;0]),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments11(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,4);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(CF - [0;2;0]),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments12(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,5);
-            [~,CF,~,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(CF - [-1;0;0]),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments13(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,2);
-            [~,~,Ixx,~] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(Ixx-8/3),testCase.epsilon);
-        end
-
-        function testWaterplaneTriangulationAndMoments14(testCase)
-            [CL,P] = waterplaneTriangulation(testCase.ModelRect243.Mesh,2);
-            [~,~,~,Iyy] = waterplaneMoments(CL,P);
-
-            testCase.verifyLessThan(abs(Iyy-32/3),testCase.epsilon);
+            testCase.verifyLessThan(abs(Sy),testCase.epsilon);
         end
     end
 end
