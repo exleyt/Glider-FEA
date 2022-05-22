c5775319a30a3bf0c6ad8710f0b3c4b15451dda1
exleyt <81730686+exleyt@users.noreply.github.com>
2022-05-21 20:00:48 -0700

    Removed unused points

--- getSurfaceSubmerged.m
+++ getSurfaceSubmerged.m
@@ -3,11 +3,11 @@ function [TO] = getSurfaceSubmerged(CL,P,EP)
 %  assuming the waterplane is z = 0
     [nP,~] = size(P);
     PE = zeros(2*nP,3);
-    PE(1:nP,:) = P;
-    pi = nP + 1;
+    PE(1:nP,:) = P; % array for extra points
+    pi = nP;
     [nCL,~] = size(CL);
-    CLE = zeros(3*nCL,3);
-    cli = 1;
+    CLE = zeros(3*nCL,3); % array for extra connections
+    cli = 0;
 
     for i = 1:nCL
         tri = P(CL(i,:),:);
@@ -16,8 +16,8 @@ function [TO] = getSurfaceSubmerged(CL,P,EP)
         sumsgn = sum(sgn);
         if sumsgn == -3 || (sumsgn == -2 && num0 == 1) ...
         || (sumsgn == -1 && num0 == 2) % all below or at waterline
-            CLE(cli,:) = CL(i,:);
             cli = cli + 1;
+            CLE(cli,:) = CL(i,:);
         elseif sumsgn == -1 || (sumsgn == 1 && num0 == 0) % 2 above/below waterline
             [~,ai] = ismember(-sign(sumsgn),sgn); % index of the lone point
             bi = mod(ai,3) + 1;
@@ -29,7 +29,7 @@ function [TO] = getSurfaceSubmerged(CL,P,EP)
             e = findZeros(a,c);
             di = -1; % index of d in PE, not tri/CL like a,b,c
             ei = -1; % index of e in PE, not tri/CL like a,b,c
-            for j = nP + 1:pi - 1 % loops over all added PEs
+            for j = nP + 1:pi % loops over all added PEs
                 if norm(d - PE(j,:)) < EP
                     di = j;
                 elseif norm(e - PE(j,:)) < EP
@@ -37,24 +37,24 @@ function [TO] = getSurfaceSubmerged(CL,P,EP)
                 end
             end
             if di == -1
+                pi = pi + 1;
                 PE(pi,:) = d;
                 di = pi;
-                pi = pi + 1;
             end
             if ei == -1
+                pi = pi + 1;
                 PE(pi,:) = e;
                 ei = pi;
-                pi = pi + 1;
             end
 
             if sumsgn == -1 
+                cli = cli + 1;
                 CLE(cli,:) = [CL(i,bi),CL(i,ci),di];
                 cli = cli + 1;
                 CLE(cli,:) = [CL(i,ci),ei,di]; % order matters for normals
+            else
                 cli = cli + 1;
-            else            
                 CLE(cli,:) = [CL(i,ai),di,ei];
-                cli = cli + 1;
             end           
         elseif sumsgn == 0 && num0 == 1
             [~,ai] = ismember(0,sgn); % index of the z = 0 point
@@ -64,27 +64,48 @@ function [TO] = getSurfaceSubmerged(CL,P,EP)
             c = tri(ci,:);
             d = findZeros(b,c);
             di = -1; % index of d in PE, not tri/CL like a,b,c
-            for j = nP + 1:pi - 1 % loops over all added PEs
+            for j = nP + 1:pi % loops over all added PEs
                 if norm(d - PE(j,:)) < EP
                     di = j;
                 end
             end
             if di == -1
+                pi = pi + 1;
                 PE(pi,:) = d;
                 di = pi;
-                pi = pi + 1;
             end
             if sgn(bi) == -1
+                cli = cli + 1;
                 CLE(cli,:) = [CL(i,ai),CL(i,bi),di];
-                cli = cli + 1;
             else
-                CLE(cli,:) = [CL(i,ai),di,CL(i,ci)];
                 cli = cli + 1;
+                CLE(cli,:) = [CL(i,ai),di,CL(i,ci)];
             end
         end
     end
-    CLN = CLE(1:cli-1,:);
-    PN = PE(1:pi-1,:);
+
+    CLFN = CLE(1:cli,:); % array of every triangle with maps to PE    
+    PFNM = zeros(pi,1); % array that maps used points in PFN
+    for i = 1:cli
+        PFNM(CLFN(i,:)) = [1,1,1];
+    end
+
+    PN = zeros(sum(PFNM),3); % array of all used points
+    PEM = zeros(pi,1); % maps each point in PE to a point in PN
+    c = 0;
+    for i = 1:pi
+        if PFNM(i) == 1
+            c = c + 1;
+            PN(c,:) = PE(i,:);
+            PEM(i) = c;
+        end
+    end
+
+    CLN = zeros(cli,3);
+    for i = 1:cli
+        CLN(i,:) = PEM(CLFN(i,:));
+    end
+
     try
         TO = triangulation(CLN,PN);
     catch
